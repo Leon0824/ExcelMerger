2686a10f66cc392a4e3b6a488e448581bc9b716a
Leon <leon0824@gmail.com>
2022-04-30 09:47:31 +0800

    Set Svelte static fallback index page

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			// default options are shown
 			pages: 'build',
 			assets: 'build',
-			fallback: null,
+			fallback: 'index.html',
 		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: '#svelte',
