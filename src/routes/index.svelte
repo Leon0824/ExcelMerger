ca13baa179a994f5a49c3873d28788e82739c4ab
Leon <leon0824@gmail.com>
2022-05-01 11:32:19 +0800

    Use Svelte event binding instead of JS event binding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,112 +6,106 @@
   import { copyFile } from "../../node_modules/@tauri-apps/api/fs";
   // import { dirname } from "../../node_modules/@tauri-apps/api/path";
 
+  function setBackgroundNormal() {
+    dropbox.classList.add("normal");
+    dropbox.classList.remove("dragenter");
+    dropbox.classList.remove("processing");
+  }
+
+  function setBackgroundDragenter() {
+    dropbox.classList.add("dragenter");
+    dropbox.classList.remove("normal");
+  }
+
+  function setBackgroundProcessing() {
+    dropbox.classList.add("processing");
+    dropbox.classList.remove("dragenter");
+    dropbox.classList.remove("normal");
+  }
+
+  async function handleFiles(files) {
+    const fileType = new Set(["xlsx", "xls"]);
+
+    for (const file of files) {
+      const fileExt = file.split(".").slice(-1)[0].toLowerCase();
+      if (!fileType.has(fileExt)) {
+        alert(`${file} 非 Excel 檔案`);
+        setBackgroundNormal();
+        return false;
+      }
+    }
+
+    const command = Command.sidecar("merger", files);
+    const output = await command.execute();
+
+    if (output.code === 0) {
+      const mergedFileTempPath = output.stdout;
+      const mergedFileName = mergedFileTempPath.split("\\").pop();
+      const defaultSavePath =
+        files[0].split("\\").slice(0, -1).join("\\") + "\\" + mergedFileName;
+      const savePath = await save({
+        defaultPath: defaultSavePath,
+      });
+
+      if (savePath) {
+        await copyFile(mergedFileTempPath, savePath);
+      }
+    }
+
+    setBackgroundNormal();
+  }
+
+  function drop() {
+    // e.stopPropagation();
+    // e.preventDefault();
+    const files = e.dataTransfer.files;
+    handleFiles(files);
+  }
+
+  async function click(e) {
+    let files = await open({
+      directory: false,
+      // filters: {
+      //   name: "*",
+      //   extensions: ["xlsx", "xls"],
+      // },
+      multiple: true,
+    });
+
+    if (typeof files === "string") {
+      files = [files];
+    }
+
+    if (files !== null) {
+      setBackgroundProcessing();
+      handleFiles(files);
+    }
+  }
+
+  listen("tauri://file-drop-hover", (e) => {
+    setBackgroundDragenter();
+  });
+
+  listen("tauri://file-drop-cancelled", (e) => {
+    setBackgroundNormal();
+  });
+
+  listen("tauri://file-drop", (e) => {
+    setBackgroundProcessing();
+    const files = e.payload;
+    handleFiles(files);
+  });
+
   if (browser) {
     document.oncontextmenu = () => {
       return false;
     };
 
     const dropbox = document.getElementById("dropbox");
-
-    function setBackgroundNormal() {
-      dropbox.classList.add("normal");
-      dropbox.classList.remove("dragenter");
-      dropbox.classList.remove('processing');
-    }
-
-    function setBackgroundDragenter() {
-      dropbox.classList.add("dragenter");
-      dropbox.classList.remove("normal");
-    }
-
-    function setBackgroundProcessing() {
-      dropbox.classList.add("processing");
-      dropbox.classList.remove("dragenter");
-      dropbox.classList.remove("normal");
-    }
-
-    async function handleFiles(files) {
-      const fileType = new Set([
-        "xlsx",
-        "xls",
-      ]);
-
-      for (const file of files) {
-        const fileExt = file.split('.').slice(-1)[0].toLowerCase();
-        if (!fileType.has(fileExt)) {
-          alert(`${file} 非 Excel 檔案`);
-          setBackgroundNormal();
-          return false;
-        }
-      }
-
-      const command = Command.sidecar("merger", files);
-      const output = await command.execute();
-
-      if (output.code === 0) {
-        const mergedFileTempPath = output.stdout;
-        const mergedFileName = mergedFileTempPath.split('\\').pop();
-        const defaultSavePath = files[0].split("\\").slice(0, -1).join('\\') + '\\' + mergedFileName;
-        const savePath = await save({
-          defaultPath: defaultSavePath,
-        });
-
-        if (savePath) {
-          await copyFile(mergedFileTempPath, savePath);
-        }
-      }
-
-      setBackgroundNormal();
-    }
-
-    async function click(e) {
-      let files = await open({
-        directory: false,
-        // filters: {
-        //   name: "*",
-        //   extensions: ["xlsx", "xls"],
-        // },
-        multiple: true,
-      });
-
-      if (typeof files === "string") {
-        files = [files];
-      }
-
-      if (files !== null) {
-        setBackgroundProcessing();
-        handleFiles(files);
-      }
-    }
-
-    function drop(e) {
-      e.stopPropagation();
-      e.preventDefault();
-      const files = e.dataTransfer.files;
-      handleFiles(files);
-    }
-
-    dropbox.addEventListener("drop", drop, false);
-    dropbox.addEventListener("click", click, false);
-
-    listen("tauri://file-drop-hover", (e) => {
-      setBackgroundDragenter();
-    });
-
-    listen("tauri://file-drop-cancelled", (e) => {
-      setBackgroundNormal();
-    });
-
-    listen("tauri://file-drop", (e) => {
-      setBackgroundProcessing();
-      const files = e.payload;
-      handleFiles(files);
-    });
   }
 </script>
 
-<svelte:head>
-</svelte:head>
+<svelte:head />
 
 <div
   id="dropbox-container"
@@ -120,6 +114,8 @@
   <div
     class="place-self-center w-full h-full row-span-full col-span-full z-10 bg-center bg-no-repeat h-screen p-16 bg-blue-50 hover:bg-blue-100 bg-contain cursor-pointer normal"
     id="dropbox"
+    on:drop={drop}
+    on:click={click}
   />
 
   <div class="row-span-full col-span-full text-center">
